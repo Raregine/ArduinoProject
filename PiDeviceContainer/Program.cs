8c56ccd69ee0a66e4803f98fb4448ae7c6ec9cc7
Raymond Regine <raregine@microsoft.com>
2021-06-17 15:03:53 +0100

    new

---
+++ PiDeviceContainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiDeviceContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
